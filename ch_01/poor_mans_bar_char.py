d281c19a5f46d708d1f961cea443bf1028f2b9f1
isaias dgr <isaiasd.garciar@gmail.com>
2021-10-25 09:51:42 -0500

    Refactor fast

--- ch_01/poor_mans_bar_char.py
+++ ch_01/poor_mans_bar_char.py
@@ -9,6 +9,12 @@ def get_poor_mans_bar_char(paragraph):
             bars[l].append(l)
     return bars
 
+def get_bar_char(paragraph):
+    bars = get_bars()
+    letters = paragraph.lower().replace(" ","")
+    [bars[item].append(item) for item in letters if letters.isalpha()]
+    return bars
+
 def get_bars():
     return {letter: [] for letter in string.ascii_lowercase}
 
@@ -16,4 +22,5 @@ if __name__ == "__main__":
     p = PrettyPrinter(2)
     paragraph = """Mr. and Mrs. Dursley of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much."""
     print(paragraph)
+    p.pprint(get_poor_mans_bar_char(paragraph))
     p.pprint(get_poor_mans_bar_char(paragraph))
